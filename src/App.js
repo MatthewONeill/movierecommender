fc010499d0b799f5b2db85ea0b39f983a411e2de
Matthew <matthewjo30@gmail.com>
2020-11-17 23:06:19 -0500

    navbar skeleton

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import BadList from './Components/BadList';
 import GoodList from './Components/GoodList'
 import GenerateMovie from './Components/GenerateMovie';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="container">
         <div className="badColumn">
           <h1>Bad List</h1>
