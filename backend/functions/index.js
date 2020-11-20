b14fed084d9718badf41532ede35bd7cb5e8fc66
Hayden Mirza <haydenjwmirza@gmail.com>
2020-11-20 14:42:11 -0500

    Added /users/create and /users/login routes. Will refactor database after lecture. /users/login needs a bit of changing.

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,16 +3,49 @@ const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const session = require('express-session')
+app.use(session({ secret: 'node run serve', cookie: { maxAge: 60000 }}))
 app.use(cors({ origin: true }));
 require('dotenv').config();
 
 //Firebase initialization
 var serviceAccount = require(process.env.FIREBASE_CONNECTION);
+//var serviceAccount = "movie-recommender-3779d-firebase-adminsdk-lw0o8-98f56ee455.json"; //For my testing purposes because im lazy.
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://movie-recommender-3779d.firebaseio.com"
 });
 const adb = admin.database();
+
+//Add user profiles
+app.post('/users/create', (req,res) => {
+  admin.auth().createUser(req.body)
+    .then(function(userRecord) {
+      console.log('Successfully created new user:', userRecord.uid);
+      req.session.userID = userRecord.uid;
+      res.sendStatus(200);
+    })
+    .catch(function(error) {
+      console.log('Error creating new user:', error);
+      res.sendStatus(400);
+    });
+});
+
+//Log in to a user profile
+app.post('/users/login', (req,res) => {
+  admin.auth().getUserByEmail(req.body.email)
+  .then(function(userRecord) {
+    console.log('Successfully fetched user data:', userRecord.toJSON());
+    req.session.userID = userRecord.uid;
+    req.session.loggedIn = true;
+    console.log(req.session);
+    res.sendStatus(200);
+  })
+  .catch(function(error) {
+   console.log('Error fetching user data:', error);
+   res.sendStatus(400);
+  });
+});
 
 //Add to the Bad List
 app.post('/badlist/add', (req,res) => {
@@ -30,7 +63,7 @@ app.post('/goodlist/add', (req,res) => {
 app.get('/badlist', (req,res) => {
   let ref = adb.ref('badMovies');
 
-  ref.once("value", (snapshot) => {
+  ref.limitToLast(10).once("value", (snapshot) => {
     let array = [];
     snapshot.forEach((child) => {
       array.push((child.val()));
@@ -46,7 +79,7 @@ app.get('/badlist', (req,res) => {
 app.get('/goodlist', (req,res) => {
   let ref = adb.ref('goodMovies');
 
-  ref.once("value", (snapshot) => {
+  ref.limitToLast(10).once("value", (snapshot) => {
     let array = [];
     snapshot.forEach((child) => {
       array.push((child.val()));
@@ -58,6 +91,6 @@ app.get('/goodlist', (req,res) => {
   });
 });
 
-
+//Allows the firebase thing to do it's stuff
 exports.app = functions.https.onRequest(app);
 //Note: run with 'node run serve'
